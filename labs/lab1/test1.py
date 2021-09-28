a23b9255a86dcf6667f30470cd9520d1a248f59d
Joselito <joselitorebello20@gmail.com>
2021-09-28 11:30:50 +0530

    added labs directory
    also added lab1 of ip

---
+++ labs/lab1/test1.py
@@ -0,0 +1,24 @@
+# program to perform all math operations ( + , - , * , / , %)
+# declare num1 and num2
+num1 = 100
+num2 = 200
+
+# Add two numbers
+result = num1 + num2
+print("Addition: ",result)
+
+# Subtract two numbers
+result = num1 - num2
+print("Subtraction: ",result)
+
+# divide two numbers
+result = num1 / num2
+print("Division: ",result)
+
+# mod operation with two numbers
+result = num1 % num2
+print("Modulo: ",result)
+
+# multiply two numbers
+result = num1 * num2
+print("Multiply: ",result)
---
+++ labs/lab1/test2.py
@@ -0,0 +1,26 @@
+# program to perform all math operations ( + , - , * , / , %)
+# program to take input of two numbers and perform the above math operations
+
+# Take num1 and num2 from the user
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+# Add two numbers
+result = num1 + num2
+print("Addition: ",result)
+
+# Subtract two numbers
+result = num1 - num2
+print("Subtraction: ",result)
+
+# divide two numbers
+result = num1 / num2
+print("Division: ",result)
+
+# mod operation with two numbers
+result = num1 % num2
+print("Modulo: ",result)
+
+# multiply two numbers
+result = num1 * num2
+print("Multiply: ",result)
