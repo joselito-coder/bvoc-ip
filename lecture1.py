84c7d833a247d5ab5ad39831338f73e0a73dbadd
Joselito <joselitorebello20@gmail.com>
2021-09-28 09:15:29 +0530

    Add lecture 1 and 2

---
+++ lecture1.py
@@ -0,0 +1,31 @@
+# Data types
+string = "string"
+floating_number = 1.16932
+integers = 1
+boolean = True
+
+# Arithmetic operations 
+a = 10
+b = 20
+# Addition
+result = a+ b
+
+# Subtraction
+result = a - b
+
+# multiplication
+result = a*  b
+
+# Division
+result = a / b
+
+# modulo
+result = a % b
+
+pi = 3.14
+radius = 10
+area = pi  * radius *radius
+print(pi * r * r)
+
+# to get the type of any variable use type() function
+type(area)
---
+++ lecture2.py
@@ -0,0 +1,6 @@
+# variable names cannot have 3 in the starting, and they are alphanumeric 
+
+# to take user input we use the input() function
+first_name = input("Enter your name : ")
+
+print("hello,",first_name)
