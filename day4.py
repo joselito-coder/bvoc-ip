455186ba6bfb6509b9c171daf3f3f0570726a649
Joselito <joselitorebello20@gmail.com>
2021-10-10 06:06:43 +0530

    Completed day4 source code

--- day4.py
+++ day4.py
@@ -1,7 +1,6 @@
 # if else conditions
 
 # Program 1
-# num = int(input("Enter a number: "))
 # Possible comparision operators that can be used with if 
 # < (less than)
 # <= (less than or equal to)
@@ -9,13 +8,37 @@
 # >= (greater than or equal to)
 # == (equal to)
 # != (not equal to)
+
+# num = int(input("Enter a number: "))
 # if num > 5:
 #     print("The number is > 5")
 # else:
 #     print("The number is not > 5")
 
 # Program 2
-# TODO
+# color = input("Enter a color: ")
+
+# if color == 'green':
+#     print("GR")
+# else:
+#     print("Not GR")
+
+# Program 3
+# c = input('Enter letter: ')
+# num = 10
+# if c == 'a':
+#     num = num + 1
+#     print("+1")
+
+# if c == 'b':
+#     num = num + 2
+#     print("+2")
+
+# if c == 'c':
+#     num = num + 3
+#     print("+3")
+
+# print(num)
 
 
 
@@ -31,44 +54,42 @@
 # else:
 #     print("a is not zero , one or two")
 
-a = 1
+# a = 1
 
-if a > 10 :
-    print('a  a > 10')
-elif a > 20:
-    print(' a > 20')
-elif a > 30:
-    print(' a > 30')
-else:
-    print("a is not Greater than 20")
-
-
-
-# last program
-day = input("Enter day")
-
-if day == 'mon':
-    print("Buy 1kg Apples")
-elif day == 'tue':
-    print('Buy 2kg Onions')
-    print('Buy 1kg Rice')
-elif day == 'wed':
-    print('Buy 2kg banana')
-elif day == 'thu':
-    print('Buy 2kg mango')
-elif day == 'fri':
-    print("Buy Ice cream")
-else:
-    print('Zomato or Swiggy')
+# if a > 10 :
+#     print('a  a > 10')
+# elif a > 20:
+#     print(' a > 20')
+# elif a > 30:
+#     print(' a > 30')
+# else:
+#     print("a is not Greater than 20")
 
 
-per = int(input("Enter percentage(%): "))
 
-if per < 0:
-    print("Invalid")
-elif per < 40 : 
-    print('Fail')
-elif per > 100:
-    print('Invalid')
-else :
-    print("Pass")
+# # last program
+# day = input("Enter day")
+# if day == 'mon':
+#     print("Buy 1kg Apples")
+# elif day == 'tue':
+#     print('Buy 2kg Onions')
+#     print('Buy 1kg Rice')
+# elif day == 'wed':
+#     print('Buy 2kg banana')
+# elif day == 'thu':
+#     print('Buy 2kg mango')
+# elif day == 'fri':
+#     print("Buy Ice cream")
+# else:
+#     print('Zomato or Swiggy')
+
+
+# per = int(input("Enter percentage(%): "))
+# if per < 0:
+#     print("Invalid")
+# elif per < 40 : 
+#     print('Fail')
+# elif per > 100:
+#     print('Invalid')
+# else :
+#     print("Pass")
