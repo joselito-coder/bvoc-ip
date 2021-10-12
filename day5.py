37ebf5126596c77d4161fe96413349ff69132ebd
Joselito <joselitorebello20@gmail.com>
2021-10-12 10:21:00 +0530

    completed day5

---
+++ day5.py
@@ -0,0 +1,87 @@
+"""
+    branching is performing a certain task based on a condition
+    new function:
+    len() ==>
+
+    String methods:
+    [stringVariable].replace(find,replace) ==> Replaces every occurences of [find] with [replace]
+
+"""
+
+# first program
+# direction = input("Enter a direction")
+# loc = 0
+# if direction == 'left':
+#     loc = loc - 1
+# elif direction == 'right':
+#     loc = loc + 1
+# else:
+#     print("Invalid")
+# print("Your character is at",loc)
+
+# # Progam 2
+# day = input("Enter Day: ")
+# if day=="Mon":
+#     print("Today is mon")
+# elif day=="Tue":
+#     print("Today is tue")
+# elif day=="Wed":
+#     print("Today is wed ")
+# elif day=="Thu":
+#     print("Today is thu")
+# elif day=="Fri":
+#     print("Today is fri")
+# elif day=="Sat":
+#     print("Today is sat")
+# elif day=="Sat":
+#     print("Today is sat")
+# else:
+#     print("Invalid")
+
+# # Program 3 
+# num = 2
+# day ="abc"
+
+# if day == 'Mon':
+#     num = num + 1
+# elif day == 'Tue':
+#     num = num * 2
+# else:
+#     num = num * 3
+
+# # program 3
+# a = 0
+# num = -1
+# if num < 0:
+#     a = a+ 1
+# if num < 30:
+#     a = a + 1
+# elif num < 90:
+#     a = a + 1
+# else:
+#     a = 300
+
+# print(a)
+
+# strings
+# strings program 1 (indexing)
+# name = "john doe"
+# l = len(name)
+# print(name[:5])
+# print(name[-1])
+
+# strings program 2 (replacemnet)
+# name1 = "john doe"
+# name2 = name1.replace("john", 'shankar')
+# print(name2)
+
+#  String concatination
+# fname = 'john'
+# lname = 'doe'
+# mname = 'jones'
+# name = fname +" " + mname+" " +lname
+# print(name)
+
+# # String multiplication
+# s = '#@'
+# print(s *3 )
