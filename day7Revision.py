f67c8921b0f1b60a804e52f33cd1906e200460f6
Joselito <joselitorebello20@gmail.com>
2021-10-23 09:23:23 +0530

    Completed day 7

---
+++ day7Revision.py
@@ -0,0 +1,22 @@
+# And operator
+a = 5
+# if a >=5:
+#     print('yes')
+# else:
+#     print('no')
+if a >= 0 and a <= 5:
+    print("yes")
+else:
+    print('no')
+
+# Or 
+a = 1 
+if a < 3 or a == 1:
+    print('yes')
+else:
+    print('no')
+
+# references:
+# Tutorial point
+# w3schols --> https://www.w3schools.com/python/
+# tutorial video https://www.youtube.com/watch?v=_uQrJ0TkZlc
