90983e2b9d686981a3a5beb6a9aa722b95883a8c
Joselito <joselitorebello20@gmail.com>
2021-11-08 10:00:19 +0530

    completed question 2

--- ca/ca1/q2.py
+++ ca/ca1/q2.py
@@ -10,14 +10,18 @@ password = input("Enter password: ")
 
 
 # Check if username or password is empty if they are empty  then print the error message
-if len(username) == 0 or len(password) == 0:
-    print("Password or username cannot be empty")
+if len(username) == 0:
+    print("username is manditory")
+elif  len(password) == 0:
+    print("password is manditory")
+# If no errors then print
 else:
     # Check for other error cases
     if len(password) > 16:
         print("password cannot be more than 16 less")
     elif len(password) < 8:
         print("password cannot be less than 8 letters")
+    # if no errors are there then print message
     else:
-        print("username and password are correct")
+        print("username and password looks good")
 
