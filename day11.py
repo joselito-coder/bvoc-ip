fc683262718215f4e916bf73fe3c01802edd6a36
Joselito <joselitorebello20@gmail.com>
2021-11-22 09:52:40 +0530

    completed day 11

---
+++ day11.py
@@ -0,0 +1,34 @@
+
+
+# shirt = 'blue'
+
+# if shirt == 'blue':
+#     print("chelsea")
+#     print("India")
+# elif shirt == "yellow":
+#     print("brazil")
+#     print("south africa")
+# elif shirt == "green":
+#     print("pakistan")
+# else:
+#     print("No match")
+
+# loop will start with 0 and end on 9
+for num in range(10):
+    print(num)
+print() 
+
+# The loop will not include the second argument
+for i in range(1,11):
+    print(i)
+print() 
+
+# The loop will take a step of 2 (meaning skip one number)
+for i in range(1,11,2):
+    print(i)
+print() 
+
+# This will reverse the loop
+# This loop means start from 10 and go to 0 (will to go 1)
+for i in range(10,0,-1):
+    print(i)
