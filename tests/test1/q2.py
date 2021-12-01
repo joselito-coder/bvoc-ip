3fe78187959a4a30344dca787aa4bb8cd1d2c0fb
joselito <joselitorebello20@gmail.com>
2021-12-01 14:39:16 +0530

    renamed test1 to mock-test-1 and added seperate section for the problems for the odd one and the problem for the even ones

--- tests/test1/q2.py
+++
@@ -1,6 +0,0 @@
-""" Question 2:
-Write a for loop to print numebrs from 2-12 (inclusive)
-"""
-
-for i in range(2,13):
-    print(i)
--- tests/test1/q3.py
+++
@@ -1,6 +0,0 @@
-""" Question 3:
-Write a for loop to print even numbers between 0 and 10 (inclusive)
-"""
-
-for i in range(0,11,2):
-    print(i)
