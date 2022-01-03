b034a6ac87448687a4f6fe9a39de733c17611df9
joselito <joselitorebello20@gmail.com>
2022-01-03 10:30:21 +0530

    completed lab 8

---
+++ labs/lab8/q1.py
@@ -0,0 +1,25 @@
+#  Q1. create 4 function (add,sub,mult,div)
+# - take 2 numbers as arguments , and return the result back the user
+
+# Define all the functions
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def div(a, b):
+    return a / b
+
+
+def mult(a, b):
+    return a * b
+
+
+# call all the functions
+add = add(1, 3)
+sub = sub(24, 23)
+mult = mult(20, 20)
+div = div(10, 2)
---
+++ labs/lab8/q2.py
@@ -0,0 +1,8 @@
+# Q2 Create a function called as calculate_area that takes raduis as parameter and returns the area
+
+def calculateArea(radius):
+    return  3.14 * (radius * radius)
+
+
+area = calculateArea(3)
+print(area)
